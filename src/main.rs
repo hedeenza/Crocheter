8adb1112f5e47815f05a64980c3a8ca4bada2e75
Zach Hedeen <hedeenza@gmail.com>
2026-06-30 11:08:22 -0400

    group sentences by paragraph utilizing the sliding window pattern, push vectors containing each paragraph into a vector

--- src/main.rs
+++ src/main.rs
@@ -31,4 +31,45 @@ fn main() {
             Err(err) => println!("[ ERROR ]: {}", err),
         }
     }
+
+    // Createa a new vector to hold the Vec<Tuple>
+    let mut all_paragraphs: Vec<Vec<String>> = Vec::new();
+
+    // Set the initial Pointer 1 index to the beginning
+    let mut pointer1_index = 0;
+
+    // While the Pointer 1 index is less than the length of the content vector...
+    while pointer1_index < input_lines.len() {
+        // If Pointer 1 hits a line with content in it...
+        if input_lines[pointer1_index].trim() != String::from("") {
+            // Create a new vector to hold the (index, line-content) tuples
+            let mut paragraph_vector: Vec<String> = Vec::new();
+            // Set Pointer 2 to where Pointer 1 is
+            let mut pointer2_index = pointer1_index;
+            // While the Pointer 2 index is less than the length of the content vector...
+            while pointer2_index < input_lines.len() {
+                // If Pointer 2 hits a blank line...
+                if input_lines[pointer2_index].trim() == String::from("") {
+                    // Push each line between Pointer 1 and Pointer 2 to a Vector
+                    for line in &mut input_lines[pointer1_index..pointer2_index] {
+                        paragraph_vector.push(line.to_string());
+                        // pointer1_index += 1;
+                    }
+                    pointer1_index = pointer2_index;
+                    break
+                }
+                // Increment Pointer 2 by One
+                pointer2_index += 1;
+            }
+            // Push the tuple vector to the output vector
+            all_paragraphs.push(paragraph_vector);
+        }
+        // Increment Pointer 1 by One
+        pointer1_index += 1;
+    }
+
+    for vector in all_paragraphs {
+        println!("{:?}", vector);
+    }
+
 }
